73dd6641205e02b412cb9303517cb1532891ff01
him666 <all.existance@gmail.com>
2020-06-23 21:31:38 -0500

    added main routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  post '/show' => 'forecasts#show'
+  get '/show' => 'forecasts#show'
+  root 'forecasts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
