03eef5defb9a47b55b84e6ea0186aabf4c80535c
him666 <all.existance@gmail.com>
2020-06-24 15:51:01 -0500

    added rescue for nil params

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -5,6 +5,7 @@ class ForecastsController < ApplicationController
   end
 
   def show
+    redirect_to root_path if params.nil? 
     @forecast = Forecast.new(params)
     @forecast_response = @forecast.cache_forecast(params[:zip_code])
     @cache_color = @forecast.weather_cache ? 'color:blue;' : 'color:black;'
